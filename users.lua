0cc525d8b33b724251d4a3f434807735cca105ce
MavricMC <118589100+MavricMC@users.noreply.github.com>
2025-01-05 17:56:01 +1300

    Create users.lua

---
+++ users.lua
@@ -0,0 +1,9 @@
+userList = {
+    {"John D", "12345", false}, --Username, password, is manager
+    {"John D2", "23456", false},
+    {"John D3", "34567", false},
+    {"John D4", "45678", false},
+    {"John D5", "56789", true},
+    {"Billy J", "abc123lol", false},
+    {"Billy J2", "lol123", true}
+}
